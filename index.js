2af4ced7845f5a5d6e58d51eba3a66dd0a1b488a
Lee Barton <hleebarton97@gmail.com>
2019-03-18 15:13:37 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'client', 'index.html')));
+
+// Render static index page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
+
+// API routes
+require('./routes/getRoutes')(app);
+
+// Listen on a port passed into node process environment
+// or port 5000 if it does not exist.
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
