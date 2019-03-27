3866c600e26bfb3998cdd4d0a3f6ae50ba27808a
Lee Barton <hleebarton97@gmail.com>
2019-03-27 11:25:21 -0500

    chatbox styling

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Render static index page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname,  'index.html'));
 });
 
 // API routes
